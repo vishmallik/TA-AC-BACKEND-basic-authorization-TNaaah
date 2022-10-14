3da8f4603b9172d80c1320aaef7999f9610d024c
visawjeet mallik <vishmallik@hotmail.com>
2022-10-14 13:06:37 +0530

    ecom website with admin dashboard assignment 2

---
+++ block-BNaaea/ecom/models/product.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    quantity: { type: Number, default: 0 },
+    price: { type: Number, default: 0 },
+    image: { type: String },
+    likes: { type: Number, default: 0 },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    isInCart: { type: Boolean, default: false },
+    category: [{ type: String }],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
